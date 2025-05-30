a4916e9c150ebb7d6c0353fc49487104f1ce6c7a
Gustavo273 <gustavo.h.lima@iesb.edu.br>
2025-05-30 20:26:32 -0300

    Cadastro, exclusao de alunos

--- Aula-13/13-crud/src/routes/AlunoForm.jsx
+++ Aula-13/13-crud/src/routes/AlunoForm.jsx
@@ -1,64 +1,139 @@
-import { StyleSheet, View } from "react-native";
-import React, { useState } from "react";
-import { Text, TextInput } from "react-native-paper";
+import { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Button, Text, TextInput } from 'react-native-paper'
+import { TextInputMask } from 'react-native-masked-text'
+import AlunoService from './AlunoService'
 
-export default function AlunoForm() {
-  const { Nome, setNome } = useState("");
-  const { Cpf, setCpf } = useState("");
-  const { Email, setEmail } = useState("");
-  const { Telefone, setTelefone } = useState("");
+export default function AlunoForm({navigation, route}) {
+
+  const [nome, setNome] = useState("")
+  const [cpf, setCpf] = useState("")
+  const [email, setEmail] = useState("")
+  const [telefone, setTelefone] = useState("")
+  const [dataNascimento, setDataNascimento] = useState("")
+
+  async function salvar() {
+    let aluno = {
+      nome,
+      cpf,
+      email,
+      telefone,
+      dataNascimento
+    }
+
+    if (!aluno.nome || !aluno.cpf || !aluno.email || !aluno.dataNascimento || !aluno.telefone) {
+      alert('Preencha todos os campos!')
+   return 
+  }
+  await AlunoService.salvar(aluno)
+  alert('Aluno cadastrado com sucesso!!')
+  navigation.reset({
+    index: 0,
+    routes: [{name: 'AlunoLista'}]
+
+  })
+  }
 
   return (
     <View style={styles.container}>
-      <Text variant="titleLarge">informe os dados do aluno</Text>
+      <Text variant='titleLarge'>Informe os dados do Aluno:</Text>
 
       <TextInput
         style={styles.input}
-        mode="outlined"
+        mode='outlined'
         label="Nome"
-        placeholder="informe o nome"
-        value={Nome}
+        placeholder='Informe o nome'
+        value={nome}
         onChangeText={setNome}
       />
+
       <TextInput
         style={styles.input}
-        mode="outlined"
+        mode='outlined'
         label="CPF"
-        placeholder="informe o CPF"
-        value={Cpf}
+        placeholder='Informe o CPF'
+        value={cpf}
         onChangeText={setCpf}
-        keyboardType="decimal-pad"
+        keyboardType='decimal-pad'
+        render={(props) => (
+          <TextInputMask 
+            {...props}
+            type={'cpf'}
+          />
+        )}
       />
+
       <TextInput
         style={styles.input}
-        mode="outlined"
+        mode='outlined'
         label="E-mail"
-        placeholder="informe o E-mail"
-        value={Email}
+        placeholder='Informe o E-mail'
+        value={email}
         onChangeText={setEmail}
+        keyboardType='email-address'
       />
-       <TextInput
+
+      <TextInput
         style={styles.input}
-        mode="outlined"
+        mode='outlined'
         label="Telefone"
-        placeholder="informe o telefone"
-        value={Telefone}
+        placeholder='Informe o Telefone'
+        value={telefone}
         onChangeText={setTelefone}
-        keyboardType="decimal-pad"
+        keyboardType='numeric'
+        render={(props) => (
+          <TextInputMask
+            {...props}
+            type={'cel-phone'}
+            options={{
+              maskType: 'BRL',
+              withDDD: true,
+              dddMask: '(99)'
+            }}
+          />
+        )}
       />
-      
+
+      <TextInput
+        style={styles.input}
+        mode='outlined'
+        label="Data de Nascimento"
+        placeholder='Informe a Data'
+        value={dataNascimento}
+        onChangeText={setDataNascimento}
+        keyboardType='numeric'
+        render={(props) => (
+          <TextInputMask
+            {...props}
+             type={'datetime'}
+             options={{
+              format: 'DD/MM/YYYY'
+             }} 
+          />
+        )}
+      />
+
+      <Button
+        style={styles.input}
+        mode='contained'
+        onPress={salvar}
+      >
+        Salvar
+      </Button>
+
+
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    marginTop: 10,
+    alignItems: 'center',
+    marginTop: 10
   },
   input: {
-    width: "90%",
-    marginTop: "10",
-  },
-});
+    width: '90%',
+    marginTop: 10
+  }
+})
--- Aula-13/13-crud/src/routes/AlunoStack.jsx
+++ Aula-13/13-crud/src/routes/AlunoStack.jsx
@@ -1,34 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator } from '@react-navigation/stack'
 
-import AlunoLista from "./AlunoLista";
-import AlunoForm from "./AlunoForm";
+import AlunoForm from './AlunoForm'
+import AlunoLista from './AlunoLista'
 
-const Stack = createStackNavigator();
-
+const Stack = createStackNavigator()
 export default function AlunoStack() {
   return (
     <Stack.Navigator>
+
       <Stack.Screen
-        name="AlunoLista"
+        name='AlunoLista'
         component={AlunoLista}
         options={{
           title: "Lista de Alunos",
-          headerTitleAlign: "center",
+          headerTitleAlign: 'center'
         }}
       />
 
       <Stack.Screen
-        name="AlunoForm"
+        name='AlunoForm'
         component={AlunoForm}
         options={{
-          title: "Formulário",
-          headerTitleAlign: "center",
+          title: "Formulário de Aluno",
+          headerTitleAlign: 'center'
         }}
       />
-    </Stack.Navigator>
-  );
-}
 
-const styles = StyleSheet.create({});
+    </Stack.Navigator>
+  )
+}
